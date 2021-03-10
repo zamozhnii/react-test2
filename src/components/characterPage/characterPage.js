372aca894df7a5f864c90c3cc85021d445b4122e
Igor Zamozhnii <zamozhnii@gmail.com>
2021-03-10 23:35:50 +0200

    src project

---
+++ src/components/characterPage/characterPage.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import ItemList from '../itemList';
+import CharDetails, {Field} from '../charDetails';
+import ErrorMessage from '../errorMessage';
+import './characterPage.css';
+import GotService from '../../services/gotService';
+import RowBlock from '../rowBlock';
+
+export default class CharacterPage extends Component {
+
+    gotService = new GotService();
+
+    state = {
+        selectedChar: 130,
+        error: false
+    }
+
+    componentDidCatch() {
+        this.setState({
+            error: true
+        })
+    }
+
+    onItemSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render() {
+
+        if(this.state.error) {
+            return <ErrorMessage/>
+        }
+
+        const itemList = (
+            <ItemList 
+                onItemSelected={this.onItemSelected}
+                getData={this.gotService.getAllCharacters} 
+                renderItem={({name, gender}) => `${name} (${gender})`} />
+        )
+
+        const charDetails = (
+            <CharDetails charId={this.state.selectedChar} >
+                <Field field='gender' label='Gender' />
+                <Field field='born' label='Born' />
+                <Field field='died' label='Died' />
+                <Field field='culture' label='Culture' />
+            </CharDetails>
+        )
+
+        return (
+            <RowBlock left={itemList} right={charDetails}/>
+        )
+    }
+}
